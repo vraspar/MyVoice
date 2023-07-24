062ccebf2b518fa1a0bfb2fc71a2e2442ec6a827
Vrajang Parikh <vrajang@outlook.com>
2023-07-24 09:51:28 -0700

    Initial Commit

---
+++ MyVoice/MyVoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyVoiceApp.swift
+//  MyVoice
+//
+//  Created by onnx training on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct MyVoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
